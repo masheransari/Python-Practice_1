72dbca3c0563930edacc5c2c2a204e1ebf049fb7
Muhammad Asher Ansari <asher.ansari@nhu.edu.pk>
2020-10-22 20:21:58 +0500

    Added 13 Task

---
+++ Task13/main.py
@@ -0,0 +1,9 @@
+from Employee import Employee
+
+employee1 = Employee(1,"Muhammad Asher Ansari",25, 25000)
+
+print(employee1.AnnualSalary())
+print(employee1.SetAge())
+print(employee1.SetSalary())
+# print(employee1.Print())
+print(employee1.print())
